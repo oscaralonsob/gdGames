8156767a5b4dff1d3dcb577db36b346dbe5e2ccd
oscaralonsob <oscaralonsob@gmail.com>
2023-12-03 17:12:53 +0100

    JumpCube: death sound even tho is not very dead sound

--- JumpCube/src/GameLoop/Systems/PlayerDeathCheckSystem.cs
+++ JumpCube/src/GameLoop/Systems/PlayerDeathCheckSystem.cs
@@ -10,17 +10,20 @@ public partial class PlayerDeathCheckSystem : ECSSystem
 			typeof(PlayerAwareComponent),
 			typeof(DeathLimitComponent),
 			typeof(Camera2D),
+			typeof(AudioStreamPlayer2D),
 		});
 	}
 
 	public override void execute(Entity entity, double delta)
 	{
 		PlayerAwareComponent playerAwareComponent = entity.GetNode<PlayerAwareComponent>("PlayerAwareComponent");
+		AudioStreamPlayer2D audioStreamPlayer2D = entity.GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
 		Camera2D camera2D = entity.GetNode<Camera2D>("Camera2D");
 
 		Node2D target = playerAwareComponent.Player;
 		float cameraSize = camera2D.GetViewport().GetVisibleRect().Size.Y / 2;
 		if (entity.Position.Y + cameraSize < target.Position.Y) {
+			audioStreamPlayer2D.Play();
 			EventBus.Instance.EmitSignal(EventBus.SignalName.PlayerKilled);
 		}
 	}
